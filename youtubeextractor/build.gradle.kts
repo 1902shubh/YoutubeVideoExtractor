acc50fbf32a9b0788956878d1de311dabf193c24
Shubham Pandey <pro.1902shubh@gmail.com>
2024-09-27 19:05:39 +0530

    version update

--- youtubeextractor/build.gradle.kts
+++ youtubeextractor/build.gradle.kts
@@ -24,17 +24,23 @@ android {
             )
         }
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 group = "com.papayacoders.youtubeextractor"
-version = "1.0.0"
+version = "1.0.1"
 
 
 
@@ -48,7 +54,7 @@ publishing {
 
             groupId = "com.papayacoders.youtubeextractor"
             artifactId = "youtubeextractor"
-            version = "1.0.0" // Ensure this matches your versioning
+            version = "1.0.1" // Ensure this matches your versioning
 
         }
     }
